3b2c7ee91056aaa6a4e54ca5743bf3355f7a351f
Kaxramonovravshan <r17ravshan@gmail.com>
2022-08-06 22:38:18 +0500

    ok

---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+let handleToggle = () => {
+    document.querySelector('.bars').classList.toggle('active')
+    document.querySelector('.accaunt-navigator-hidden-container').classList.toggle('active')
+    document.querySelector('.accaunt-navigator-hidden').classList.toggle('active')
+}
